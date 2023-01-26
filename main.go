13e4fc89e627ae6d05ecc936493e80d658d6ba8a
Dele Amefo <creditamefo@gmail.com>
2023-01-26 23:57:13 +0100

    added tickets

--- main.go
+++ main.go
@@ -3,6 +3,10 @@ package main
 func main() {
 	println("Hello world")
 
-	var  conferenceName = "Go conference" 
+	var conferenceName = "Go conference"
 	const conferenceTickets = 50
+	var remainingTickets = 50
+
+	print("Welome to %v booking application", conferenceName)
+	print("We have %v tickets remaining", remainingTickets)
 }
