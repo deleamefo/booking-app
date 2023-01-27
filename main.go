24edb70b92df71fc3854baeffbb88d0017ae978d
Dele Amefo <creditamefo@gmail.com>
2023-01-27 20:44:02 +0100

    created user profile

--- main.go
+++ main.go
@@ -5,10 +5,21 @@ import "fmt"
 func main() {
 
 	conferenceName := "Go conference"
-	const conferenceTickets = 50
-	var remainingTickets = 23
+	const conferenceTickets int = 50
+	var remainingTickets uint = 23
 
 	fmt.Printf("Welome to %v booking application\n", conferenceName)
 	fmt.Printf("We have total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
-	fmt.Printf("Get your tickets here to attend")
+	fmt.Println("Get your tickets here to attend")
+
+	var userName string
+	var userTickets int
+	// ask user for their name
+
+	fmt.Println("Enter your first name:")
+	fmt.Scan(&userName)
+
+	userTickets = 2
+	fmt.Printf("User %v booked %v tickets\n", userName, userTickets)
+
 }
