bb97134c5abdf5170daab111388c54c8cea040e8
Dele Amefo <creditamefo@gmail.com>
2023-02-17 18:14:44 +0100

    Added user validation

--- main.go
+++ main.go
@@ -39,35 +39,40 @@ func main() {
 
 		// use this (remainingTickets - uint(userTickets) =..... or place uint on userTickets as 'var userTickets uint')
 
-		if userTickets > remainingTickets {
-			fmt.Printf("We only have %v tickets remaining , so you can't book %v tickets\n", remainingTickets, userTickets)
-			break
-		}
+		isValidName := len(firstName) >= 2 && len(lastName) >= 2
+		isValidEmail := strings.Contains(email, "@")
+		isValidTicketNumber := userTickets > 0 && userTickets <= remainingTickets
 
-		remainingTickets = remainingTickets - userTickets
-		bookings = append(bookings, firstName+" "+lastName)
+		if isValidName && isValidEmail && isValidTicketNumber {
+			remainingTickets = remainingTickets - userTickets
+			bookings = append(bookings, firstName+" "+lastName)
 
-		fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+			fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
 
-		fmt.Printf("%v tickets reamining for %v\n", remainingTickets, conferenceName)
+			fmt.Printf("%v tickets reamining for %v\n", remainingTickets, conferenceName)
 
-		firstNames := []string{}
-		for _, booking := range bookings { // ( _ = index)
-			var names = strings.Fields(booking)
-			firstNames = append(firstNames, names[0])
-		}
-		fmt.Printf("The first names of bookings are : %v\n", firstNames)
+			firstNames := []string{}
+			for _, booking := range bookings { // ( _ = index)
+				var names = strings.Fields(booking)
+				firstNames = append(firstNames, names[0])
+			}
+			fmt.Printf("The first names of bookings are : %v\n", firstNames)
 
-		// noTicketsRemaining := remainingTickets == 0
-		// if noTicketsRemaining { // no tickets remaining
-		// 	// end program
-		// 	fmt.Println("We have no tickets remaining. Come back next year!")
-		// 	break
+			// noTicketsRemaining := remainingTickets == 0 // Do this when the handling more than one if stmt
+			// if noTicketsRemaining { // no tickets remaining
+			// 	// end program
+			// 	fmt.Println("We have no tickets remaining. Come back next year!")
+			// 	break
 
-		if remainingTickets == 0 { // no tickets remaining
-			// end program
-			fmt.Println("We have no tickets remaining, please come next year!")
-			break
+			if remainingTickets == 0 { // no tickets remaining
+				// end program
+				fmt.Println("We have no tickets remaining, please come next year!")
+				break
+			}
+
+		} else {
+			fmt.Printf("We only have %v tickets remaining, so you can't book %v tickets\n", remainingTickets, userTickets)
+			//continue
 		}
 
 	}
