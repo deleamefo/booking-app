5210baa50f062fd3b1e506cd3deee8c44b85cc9f
Dele Amefo <creditamefo@gmail.com>
2023-03-02 23:43:24 +0100

    Refactoring...helper.go file

--- helper.go
+++ helper.go
@@ -4,7 +4,7 @@ import "strings"
 
 func validateUserInput(firstName string, lastName string, email string, userTickets uint) (bool, bool, bool) {
 	isValidName := len(firstName) >= 2 && len(lastName) >= 2
-	isValidEmail := strings.Contains(email, "@")
+	isValidEmail := strings.Contains(email, "@") // check the keyword 'Contains'
 	isValidTicketNumber := userTickets > 0 && userTickets <= remainingTickets
 	return isValidName, isValidEmail, isValidTicketNumber
 
